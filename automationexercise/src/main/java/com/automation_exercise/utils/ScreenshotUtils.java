8f0e2e89bfd744515acd498fb09c0a62ad08b2e9
Srinjoy Sur <srinjoy_sur@epam.com>
2025-10-22 15:59:03 +0530

    feat: feat common and util project files and folders created

---
+++ automationexercise/src/main/java/com/automation_exercise/utils/ScreenshotUtils.java
@@ -0,0 +1,21 @@
+package com.automation_exercise.utils;
+
+import com.contact_list.constants.ConstantsProvider;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ScreenshotUtils {
+    private static volatile TakesScreenshot screenshot;
+
+    public static void saveScreenshot(WebDriver driver,String name) throws IOException {
+        screenshot = (TakesScreenshot) driver;
+        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File destFile = new File(ConstantsProvider.screenshotsDirPath + "/failure" + name + "_screenshot.png");
+        FileHandler.copy(srcFile, destFile);
+    }
+}
---
+++ automationexercise/src/main/java/com/automation_exercise/utils/WebDriverUtils.java
@@ -0,0 +1,29 @@
+package com.automation_exercise.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+
+public class WebDriverUtils {
+
+    public static void click(WebElement element) {
+        element.click();
+    }
+
+    public static void sendKeys(WebElement element, String keys) {
+        element.sendKeys(keys);
+    }
+
+    public static String getText(WebElement element) {
+        return element.getText();
+    }
+
+    public static String getCurrentUrl(WebDriver driver) {
+        return driver.getCurrentUrl();
+    }
+
+    public static void waitForVisibility(Wait wait, WebElement element){
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+}
