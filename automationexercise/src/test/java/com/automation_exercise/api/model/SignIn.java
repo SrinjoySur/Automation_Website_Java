1a049e1b273877c02bdd906bf29f8bc07ac57c57
Srinjoy Sur <srinjoy_sur@epam.com>
2025-10-22 16:02:11 +0530

    feat: signin model created

---
+++ automationexercise/src/test/java/com/automation_exercise/api/model/SignIn.java
@@ -0,0 +1,29 @@
+package com.automation_exercise.api.model;
+
+import lombok.Data;
+
+@Data
+public class SignIn {
+    private String email;
+    private String password;
+    public SignIn(SignInBuilder builder){
+        this.email=builder.email;
+        this.password=builder.password;
+    }
+    public static class SignInBuilder {
+        private String email;
+        private String password;
+
+        public SignInBuilder withEmail(String email) {
+            this.email=email;
+            return this;
+        }
+        public SignInBuilder withPassword(String password){
+            this.password=password;
+            return this;
+        }
+        public SignIn build(){
+            return new SignIn(this);
+        }
+    }
+}
