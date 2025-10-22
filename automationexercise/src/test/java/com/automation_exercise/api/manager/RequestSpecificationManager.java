efe281340b83e10f04015be0dc8ba833880462c3
Srinjoy Sur <srinjoy_sur@epam.com>
2025-10-22 16:00:57 +0530

    feat: API managers created

---
+++ automationexercise/src/test/java/com/automation_exercise/api/manager/RequestSpecificationManager.java
@@ -0,0 +1,35 @@
+package com.automation_exercise.api.manager;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static com.automation_exercise.constants.ConstantsProvider.BASE_URI;
+
+public class RequestSpecificationManager {
+    private static RequestSpecification requestSpec;
+
+    public static RequestSpecification getRequestSpecification() {
+        if (requestSpec == null) {
+            requestSpec = new RequestSpecBuilder()
+                    .setBaseUri(BASE_URI)
+                    .setContentType(ContentType.JSON)
+                    .build();
+        }
+        return requestSpec;
+    }
+    public static RequestSpecification getAuthenticatedRequestSpecification(String token) {
+        if (requestSpec == null) {
+            requestSpec = new RequestSpecBuilder()
+                    .addHeader("Authorization", "Bearer " + token)
+                    .setBaseUri(BASE_URI)
+                    .setContentType(ContentType.JSON)
+                    .build();
+        }
+        return requestSpec;
+    }
+
+    public static void resetRequestSpec() {
+        requestSpec = null;
+    }
+}
---
+++ automationexercise/src/test/java/com/automation_exercise/api/manager/ResponseSpecificationManager.java
@@ -0,0 +1,4 @@
+package com.automation_exercise.api.manager;
+
+public class ResponseSpecificationManager {
+}
